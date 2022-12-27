fc6793c13c9783af6680332401144195a2b9f31a
Alina <93431239+Alina0101@users.noreply.github.com>
2022-12-27 17:28:57 +0300

    Created CRUD for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,30 +1,29 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: %i[update show destroy edit]
+
   def create
     recipe = Recipe.create(recipe_params)  
 
-    redirect_to recipes_path(recipe)
+    redirect_to recipe_path(recipe)
   end 
   
   def update
-    @recipe = Recipe.find(params[:id])
     @recipe.update(recipe_params)
 
-    #redirect_to recipes_path(@recipe)
+    redirect_to recipe_path(@recipe)
   end
 
   def destroy
-    @recipe = Recipe.find(params[:id])
     @recipe.destroy
 
-    redirect_to recipe_path
+    redirect_to recipes_path
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def index
-    @recipe = Recipe.all
+    @recipes = Recipe.all
   end
 
   def new
@@ -32,12 +31,14 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
   end
 
   private
-
   def recipe_params
     params.require(:recipe).permit(:title, :body, :user)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
    root "welcome#index"
-   resource :recipes
+   resources :recipes
    resources :users, only: %i[new create]
    resource :session, only: %i[new create destroy]
    
-
 end
