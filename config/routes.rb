4d867b51bc702b5ba5cb098078c3e76a73bc8efb
AnastasiaDb <82052788+AnastasiaDb@users.noreply.github.com>
2022-12-27 18:34:40 +0300

    create destroy for users
    Co-Authored-By: Kosten-73 <49977127+Kosten-73@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
    root "welcome#index"
-   resources :users, only: %i[new create]
+   resources :users, only: %i[new create edit update destroy]
    resource :session, only: %i[new create destroy]
    
 end
