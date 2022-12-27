faea95524d4a69cfed0ed5167c3e5dbf9f056e7e
FViKki07 <73124000+FViKki07@users.noreply.github.com>
2022-12-27 18:39:06 +0300

    edit validates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,23 @@ class UsersController < ApplicationController
 
   def create
     user_params = params.require(:user).permit(:name, :login, :password)
-    user = User.find_by(login: user_params[:login])
+    @user = User.find_by(login: user_params[:login])
 
-    if user.present?
-      flash.now[:alert] = 'Такой логин уже есть на сайте'
-       redirect_to '/users/new'
-     # redirect_to 
-    else
-      User.create(user_params)
+    if @user.save
       redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    else
+      flash.now[:alert] = 'Неправильно заполнили форму регистрации'
+      
+      render :new
     end
+    #if user.present?
+     # flash.now[:alert] = 'Такой логин уже есть на сайте'
+      # redirect_to '/users/new'
+     # redirect_to 
+    #else
+      #User.create(user_params)
+      #redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    #end
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
     has_secure_password
     validates :user, presence: true
-    validates :login, format: { with: /[A-Za-z0-9]+@{1}@\.\w+/ , message: 'Неправильный логин'}
-    validate :validate_for_login
+    validates :login, presence: true, uniqueness: true
+    #validates :login, format: { with: /[A-Za-z0-9]+@{1}@\.\w+/ , message: 'Неправильный логин'}
+    #validate :validate_for_login
     
-    def validate_for_login
-        user = User.find_by(login: :login)
-        if user.present?
-          errors.add(:login, 'Такой логин уже есть')
-        end
-    end
+    #def validate_for_login
+     #   user = User.find_by(login: :login)
+      #  if user.present?
+     #     errors.add(:login, 'Такой логин уже есть')
+    #    end
+   # end
 end
